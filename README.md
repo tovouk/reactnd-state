b457e44e7f63664f8b708c5ac3f1af750b60c569
Hectic Envek <burningremedy@gmail.com>
2020-07-02 18:46:44 -0500

    create action type contstants and update readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Description
 
 This is a code along introducing various state management approaches for stores
+
+WARNING: If you somehow find yourself here and have a TODO plugin in vscode, you are likely to pick some false todo items
--- index.js
+++ index.js
@@ -34,15 +34,20 @@ function createStore(reducer) {
 }
 
 //App code
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
 
 //to-dos reducer
 function todos (state = [], action) { 
     switch(action.type){
-        case 'ADD_TODO':
+        case ADD_TODO:
             return state.concat([action.todo])
-        case 'REMOVE_TODO':
+        case REMOVE_TODO:
             return state.filter((todo)=> todo.id !== action.id)
-        case 'TOGGLE_TODO':
+        case TOGGLE_TODO:
             return state.map((todo)=> todo.id !== action.id? todo: Object.assign({},todo,{complete: !todo.complete}))
         default:
             return state
@@ -51,9 +56,9 @@ function todos (state = [], action) {
 //Goals Reducer
 function goals (state = [], action){
     switch(action.type){
-        case 'ADD_GOAL':
+        case ADD_GOAL:
             return state.concat([action.goal])
-        case 'REMOVE_GOAL':
+        case REMOVE_GOAL:
             return state.filter((goal)=> goal.id !== action.id)
         default:
             return state
@@ -66,13 +71,16 @@ function app(state = {},action) {
         goals: goals(state.goals, action)
     }
 }
+/*
+
+Use the below to test this store
 
 const store = createStore(app)
 store.subscribe(()=>{
     console.log('The new state is: ', store.getState())
 })
 store.dispatch({
-    type: 'ADD_TODO',
+    type: ADD_TODO,
     todo:{
         id: 0,
         name: 'Learn Redux',
@@ -80,7 +88,7 @@ store.dispatch({
     }
 })
 store.dispatch({
-    type: 'ADD_TODO',
+    type: ADD_TODO,
     todo:{
         id: 1,
         name: 'Tie my shoes',
@@ -88,7 +96,7 @@ store.dispatch({
     }
 })
 store.dispatch({
-    type: 'ADD_TODO',
+    type: ADD_TODO,
     todo:{
         id: 2,
         name: 'Read a book',
@@ -96,28 +104,29 @@ store.dispatch({
     }
 })
 store.dispatch({
-    type: 'REMOVE_TODO',
+    type: REMOVE_TODO,
     id: 1
 })
 store.dispatch({
-    type: 'TOGGLE_TODO',
+    type: TOGGLE_TODO,
     id: 2
 })
 store.dispatch({
-    type: 'ADD_GOAL',
+    type: ADD_GOAL,
     goal:{
         id: 0,
         name: 'Be cool'
     }
 })
 store.dispatch({
-    type: 'ADD_GOAL',
+    type: ADD_GOAL,
     goal:{
         id: 0,
         name: 'Be awesome'
     }
 })
 store.dispatch({
-    type: 'REMOVE_GOAL',
+    type: REMOVE_GOAL,
     id: 0
 })
+*/
