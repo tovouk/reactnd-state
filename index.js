26b924c75241b8d731074ba87ce9f384702049d3
Hectic Envek <burningremedy@gmail.com>
2020-07-02 18:19:03 -0500

    Add more action handling to reducer

--- index.js
+++ index.js
@@ -34,12 +34,17 @@ function createStore(reducer) {
 }
 
 //App code
-function todos (state = [], action) {
-    if(action.type === 'ADD_TODO'){
-        return state.concat([action.todo])
+function todos (state = [], action) { 
+    switch(action.type){
+        case 'ADD_TODO':
+            return state.concat([action.todo])
+        case 'REMOVE_TODO':
+            return state.filter((todo)=> todo.id !== action.id)
+        case 'TOGGLE_TODO':
+            return state.map((todo)=> todo.id !== action.id? todo: Object.assign({},todo,{complete: !todo.complete}))
+        default:
+            return state
     }
-
-    return state
 }
 
 const store = createStore(todos)
