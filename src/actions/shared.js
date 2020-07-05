473285a373fa731628ff1d4d80d2c8530cc24520
Hectic Envek <burningremedy@gmail.com>
2020-07-05 17:26:44 -0500

    Begin organizing action creators in separate files

---
+++ src/actions/shared.js
@@ -0,0 +1,23 @@
+import API from 'goals-todos-api'
+
+const RECEIVE_DATA = 'RECEIVE_DATA'
+
+export function receiveData (todos,goals){
+    return {
+        type: RECEIVE_DATA,
+        todos,
+        goals
+    }
+}
+
+export function handleInitialData() {
+    return (dispatch) => {
+        Promise.all([
+        API.fetchTodos(),
+        API.fetchGoals()
+        ])
+        .then(([ todos, goals ]) => {
+            dispatch(receiveData(todos,goals))
+        })
+    }
+}
