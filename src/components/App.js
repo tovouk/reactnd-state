cdd16d29d4eb13d5daaa848e8dba91a0ea918004
Hectic Envek <burningremedy@gmail.com>
2020-07-05 17:48:03 -0500

    Move components to own files

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import {connect} from 'react-redux'
+import ConnectedTodos from './Todos'
+import ConnectedGoals from './Goals'
+import {handleInitialData} from '../actions/shared'
 
 function App() {
-  return (
-    <div>
-      Hello World
-    </div>
-  );
+  componentDidMount(){
+    const {dispatch} = this.props
+    dispatch(handleInitialData())
+  }
+
+  render (){
+
+      if(this.props.loading === true){
+          return <h3>loading</h3>
+      }
+
+      return (
+          <div>
+              <ConnectedTodos />
+              <ConnectedGoals/>
+          </div>
+      )
+  }
 }
 
-export default App;
+export default connect((state)=>({
+  loading: state.loading
+}))(App)
